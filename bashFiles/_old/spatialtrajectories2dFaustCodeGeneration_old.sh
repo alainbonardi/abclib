969a172c071b4ccf4b580bfd000c76f65def0074
Alain Bonardi <alain.bonardi@gmail.com>
2021-06-09 23:06:34 +0200

    Generated the spatial trajectories .dsp

--- bashFiles/_old/spatialtrajectories2dFaustCodeGeneration_old.sh
+++ bashFiles/_old/spatialtrajectories2dFaustCodeGeneration_old.sh
@@ -7,6 +7,9 @@ mkdir abc_2d_spatialtrajectories
 cd abc_2d_spatialtrajectories/
 #no parameter is necessary for these spatial trajectories dsp files
 headerfilename="../../bashFiles/faustCodeHeader.txt"
+associatedcommonfilename="../abccommon/abc2dspatialtrajectories.dsp"
+utilityfilename1="../abccommon/abcutilities/abcplayer.dsp"
+utilityfilename2="../abccommon/abcutilities/abclines.dsp"
 #
 #abc_squareandztrajectory.dsp
 #
@@ -18,9 +21,18 @@ do
 done <"$headerfilename"
 #writes the declared name
 echo "declare name \"abc_2d_squareandztrajectory\";" >> $sortie
-#writes the process line
+#writes the associated common file
+while IFS= read -r line
+do
+    echo "$line" >> $sortie
+done <"$associatedcommonfilename"
+#writes the other common file (utility functions)
+while IFS= read -r line
+do
+    echo "$line" >> $sortie
+done <"$utilityfilename1"
 echo "//
-process = library(\"abc.lib\").abc_2d_squareAndZTrajectory;" >> $sortie
+process = squareAndzTrajectory2d;" >> $sortie
 #
 #abc_squaretrajectory.dsp
 #
@@ -32,9 +44,18 @@ do
 done <"$headerfilename"
 #writes the declared name
 echo "declare name \"abc_2d_squaretrajectory\";" >> $sortie
-#writes the process line
+#writes the associated common file
+while IFS= read -r line
+do
+    echo "$line" >> $sortie
+done <"$associatedcommonfilename"
+#writes the other common file (utility functions)
+while IFS= read -r line
+do
+    echo "$line" >> $sortie
+done <"$utilityfilename1"
 echo "//
-process = library(\"abc.lib\").abc_2d_squareTrajectory;" >> $sortie
+process = squareTrajectory2d;" >> $sortie
 #
 #abc_ztrajectory.dsp
 #
@@ -46,9 +67,18 @@ do
 done <"$headerfilename"
 #writes the declared name
 echo "declare name \"abc_2d_ztrajectory\";" >> $sortie
-#writes the process line
+#writes the associated common file
+while IFS= read -r line
+do
+    echo "$line" >> $sortie
+done <"$associatedcommonfilename"
+#writes the other common file (utility functions)
+while IFS= read -r line
+do
+    echo "$line" >> $sortie
+done <"$utilityfilename1"
 echo "//
-process = library(\"abc.lib\").abc_2d_zTrajectory;" >> $sortie
+process = zTrajectory2d;" >> $sortie
 #
 #abc_randomtrajectory.dsp
 #
@@ -60,6 +90,15 @@ do
 done <"$headerfilename"
 #writes the declared name
 echo "declare name \"abc_2d_randomtrajectory\";" >> $sortie
-#writes the process line
+#writes the associated common file
+while IFS= read -r line
+do
+    echo "$line" >> $sortie
+done <"$associatedcommonfilename"
+#writes the other common file (utility functions)
+while IFS= read -r line
+do
+    echo "$line" >> $sortie
+done <"$utilityfilename2"
 echo "//
-process = library(\"abc.lib\").abc_2d_randomTrajectory;" >> $sortie
+process = randomTrajectory2d(freq, size);" >> $sortie
