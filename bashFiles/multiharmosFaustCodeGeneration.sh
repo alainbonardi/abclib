e6bc58f068a9772cf10a1097f6e18c53e377bf69
Alain Bonardi <alain.bonardi@gmail.com>
2021-10-16 17:23:15 +0200

    re-design of multiple harmonizers

--- bashFiles/multiharmosFaustCodeGeneration.sh
+++ bashFiles/multiharmosFaustCodeGeneration.sh
@@ -23,10 +23,6 @@ fi
 let "Nch = 2 * $amborder + 2"
 #creates an array of harmonizers
 headerfilename="../../bashFiles/faustCodeHeader.txt"
-associatedcommonfilename="../abccommon/abcmultiharmo.dsp"
-utilityfilename1="../abccommon/abcutilities/abclines.dsp"
-utilityfilename2="../abccommon/abcutilities/abcsinenv.dsp"
-utilityfilename3="../abccommon/abcutilities/abcdbcontrol.dsp"
 #
 for i in `seq 1 $Nch`
 do
@@ -38,26 +34,7 @@ do
     done <"$headerfilename"
 #writes the declared name
 echo "declare name \"abc_harmo$i\";" >> $sortie
-#writes the associated common file
-    while IFS= read -r line
-    do
-        echo "$line" >> $sortie
-    done <"$associatedcommonfilename"
-#writes the other common file (utility functions)
-    while IFS= read -r line
-    do
-        echo "$line" >> $sortie
-    done <"$utilityfilename1"
-#writes the other common file (utility functions)
-    while IFS= read -r line
-    do
-        echo "$line" >> $sortie
-    done <"$utilityfilename2"
-#writes the other common file (utility functions)
-    while IFS= read -r line
-    do
-        echo "$line" >> $sortie
-    done <"$utilityfilename3"
+#writes the process line
 echo "//
-process = blockOfHarmonizers($i);" >> $sortie
+process = library(\"abc.lib\").abc_multiharmo_obj($i);" >> $sortie
 done
