c524a8aaf86e7e5e3c80c4bb4b1f3c87ba85389f
Paul Goutmann <paul.goutmann@gmail.com>
2025-02-28 16:19:15 +0100

    Add a condition for abc.hoa.binaural~ in the wrapper: because of the compilation pipeline those objects have 1 outlet more than the others

--- forMaxLibrary/javascript/abc_wrp.js
+++ forMaxLibrary/javascript/abc_wrp.js
@@ -550,7 +550,17 @@ function patching() {
 	var outlet = patcher.newdefault(20, 420, "outlet");
 	outlet.message('comment','(multi-channel signal) Output');
 
-	if (inletsoutlets(abcObject)[1] > 1) {
+	//Because of the compilation pipeline binaural objects can't be compiled with RNBO
+	//Instead of that we compile it directly with faust command but there are 1 outlet more than the other objects
+	if (patcherName = "abc.hoa.binaural~") {
+		objectToInstantiate = "mc.pack~ " + (inletsoutlets(abcObject)[1]-1);
+		var packer = patcher.newdefault(20, 360, objectToInstantiate);
+
+		for (d = 0; d < (inletsoutlets(abcObject)[1]-1); d++) {
+			connectobject(abcObject, d, packer, d);//connect abc object to mc.pack~
+		}
+		connectobject(packer, 0, outlet, 0);//connect mc.pack~ to outlet
+	} else if (inletsoutlets(abcObject)[1] > 1) {
 		objectToInstantiate = "mc.pack~ " + (inletsoutlets(abcObject)[1]);
 		var packer = patcher.newdefault(20, 360, objectToInstantiate);
 		//addobjectauto(objectToInstantiate, 4);
