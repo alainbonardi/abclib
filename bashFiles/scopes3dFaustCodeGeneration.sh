b29a67d21eab80813b13e67a099a94d15859fc79
Alain Bonardi <alain.bonardi@gmail.com>
2026-08-24 14:51:04 +0200

    Added bash file to generate abc_3d_scope$i.dsp files Added abc_3d_scope_ui(N) function to abc.lib

--- bashFiles/scopes3dFaustCodeGeneration.sh
+++ bashFiles/scopes3dFaustCodeGeneration.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-#ABC_2D_SCOPES FAUST CODE GENERATION
-cd ../faustCodes/
-#deletes the previous abc_2d_scopes folder
-rm -R abc_2d_scopes
-mkdir abc_2d_scopes
-cd abc_2d_scopes/
+#ABC_3D_SCOPES FAUST CODE GENERATION
+cd $HOME/Documents/Github/abclib/faustCodes/
+#deletes the previous abc_3d_scopes folder
+rm -R abc_3d_scopes
+mkdir abc_3d_scopes
+cd abc_3d_scopes/
 #is there a parameter?
-#if not we force 7 as default value
+#if not we force 3 as default value
 if [ -z $1 ]
 then
-    amborder=7
+    amborder=3
 else
     amborder=$1
 fi
-#is the parameter lower than 7
-#we then force 7 as a minimum
-if [ $amborder -le 7 ]
+#is the parameter lower than 3
+#we then force 3 as a minimum
+if [ $amborder -le 3 ]
 then
-    amborder=7
+    amborder=3
 fi
 headerfilename="../../bashFiles/faustCodeHeader.txt"
 for i in `seq 1 $amborder`
 do
-let "j = 2 * $i + 2"
     sortie="abc_3d_scope$i.dsp"
 #writes the header
     while IFS= read -r line
@@ -30,7 +29,7 @@ let "j = 2 * $i + 2"
         echo "$line" >> $sortie
     done <"$headerfilename"
 #writes the declared name
-echo "declare name \"abc_3d_scope$i\";" >> $sortie
+echo "declare name \"abc_2d_scope$i\";" >> $sortie
 #writes the process line
 echo "//
 process = library(\"abc.lib\").abc_3d_scope_ui($i);" >> $sortie
